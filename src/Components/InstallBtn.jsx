8cfaa00e6c3a30fc75c4bcf77b72bc383f2aedb6
Gopikannan K <gopik.kumaresan@gmail.com>
2026-01-01 12:27:45 +0530

    use state and use effect defined

--- src/Components/InstallBtn.jsx
+++ src/Components/InstallBtn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/style.css";
 import { usePWAInstall } from "../Hooks/usePWAInstall";
 const InstallButton = () => {
