c9050ff56951754346ebdf7da81ddae3cc79bff4
Gopikannan K <gopik.kumaresan@gmail.com>
2026-01-01 12:23:58 +0530

    bug fix -- change for laptop

--- src/Components/InstallBtn.jsx
+++ src/Components/InstallBtn.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-import '../Styles/style.css'
-import InstallBanner from "./InstallBanner";
+import "../Styles/style.css";
+import { usePWAInstall } from "../Hooks/usePWAInstall";
 const InstallButton = () => {
+  const { isInstallable, installApp } = usePWAInstall();
+  const [appName, setAppName] = useState("My Portfolio");
+  const [iconUrl, setIconUrl] = useState("");
 
+  useEffect(() => {
+    fetch("/manifest.webmanifest")
+      .then((res) => res.json())
+      .then((data) => {
+        setAppName(data.short_name || data.name);
+        setIconUrl(data.icons[0].src); // pick first icon
+      });
+  }, []);
+
+  if (!isInstallable) return null;
   return (
     <div className="install-modal-backdrop">
       <div className="install-modal">
-      <InstallBanner/>
+        <div className="pwa-banner">
+          <img src={iconUrl} alt={appName} width={40} />
+          <div>
+            <strong>{appName}</strong>
+            <p>Install {appName} for offline & faster experience!</p>
+          </div>
+          <button onClick={installApp}>Install</button>
+        </div>
       </div>
     </div>
   );
